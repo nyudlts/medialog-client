18f845e1fd38cb63d0e30da8720f8c63ff3bd4a6
Don Mennerich <don.mennerich@nyu.edu>
2024-09-17 09:44:50 -0400

    adding function to get entry uuids for a resource

--- Entries.go
+++ Entries.go
@@ -60,3 +60,30 @@ func (mlc MedialogClient) GetEntryUUIDs() ([]string, error) {
 	return uuids, nil
 
 }
+
+func (mlc MedialogClient) GetEntryUUIDsForResource(resourceID int) ([]string, err) {
+	reqURL := fmt.Sprintf("%s/resources/:%d/entries?all_ids=true", mlc.BaseURL, resourceID)
+	req, err := http.NewRequest("GET", reqURL, nil)
+	if err != nil {
+		return []string{}, err
+	}
+	req.Header.Add("X-Medialog-Token", mlc.Token)
+
+	resp, err := mlc.Client.Do(req)
+	if err != nil {
+		return []string{}, err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return []string{}, err
+	}
+
+	uuids := []string{}
+	if err := json.Unmarshal(body, &uuids); err != nil {
+		return uuids, err
+	}
+
+	return uuids, nil
+
+}
